0adce75e73eb4c2936861a036645fc2a2fde1c1a
duxiaodong <duxiaodong@ddjucai.com>
2015-03-10 22:33:36 +0800

    订单管理

--- apps/business/urls.py
+++ apps/business/urls.py
@@ -3,4 +3,8 @@ import views
 
 urlpatterns = patterns('',
     url(r'^profile$', views.business_profile, name="business_profile"),
+    url(r'^create$', views.order_create, name="order_create"),
+    url(r'^manage$', views.order_manage, name="order_manage"),
+    url(r'^order$', views.order_detail, name="order_detail"),
+    url(r'^collect$', views.collect, name="collect"),
 )
--- apps/business/views.py
+++ apps/business/views.py
@@ -6,3 +6,15 @@ from django.shortcuts import render_to_response
 
 def business_profile(request):
 	return render_to_response("business/business_profile.html")
+
+def order_create(request):
+	return render_to_response("business/order_create.html")
+
+def order_manage(request):
+	return render_to_response("business/order_manage.html")
+
+def order_detail(request):
+	return render_to_response("business/order_detail.html")
+
+def collect(request):
+	return render_to_response("business/collect.html")
